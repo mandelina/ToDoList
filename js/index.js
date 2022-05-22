2a2499db0199c02ae16c315db9e9b91986e4b785
minju moon <mmj9908@gmail.com>
2022-05-22 20:59:05 +0900

    삭제구현

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
-const $input = document.querySelector(".todo_input"); //할일입력$input박스
+const $input = document.querySelector(".todo_input"); //할일입력 $input박스
 const todoListElem = document.querySelector(".list"); //ul
 
 let todos = [];
 let id = 0;
 
+// 현재 todo를 업데이트 하는 함수
 const setTodos = (newTodos) => {
   todos = newTodos;
 };
 
+// 현재 todo를 가져오는 함수
 const getAllTodos = () => {
   return todos;
 };
 
+//todo를 추가하는 함수
 const appendTodos = (text) => {
   const newid = id++;
   const newTodos = getAllTodos().concat({
@@ -23,37 +26,52 @@ const appendTodos = (text) => {
   paintTodos();
 };
 
+//todos를 삭제하는 함수
+const deleteTodo = (todoId) => {
+  console.log("click!");
+  const newTodos = getAllTodos().filter((todo) => todo.id !== todoId); //id가 같은 todo를 제외하고 새로운 todo에 넣어줌
+  setTodos(newTodos);
+  paintTodos(); //다시 렌더링
+};
+
 //할일이 추가될때 렌더링해주는 함수
 const paintTodos = () => {
   todoListElem.innerHTML = null; //todoListElem 요소안의 html을 초가화
   const allTodos = getAllTodos(); //배열 가져오기
   allTodos.forEach((todo) => {
+    // 추가할 li
     const todoItemElem = document.createElement("li");
+
+    // 할일 text
     const todoEle = document.createElement("label");
     todoEle.classList.add("txt");
     todoEle.textContent = todo.content;
 
+    // 삭제 버튼
     const delBtn = document.createElement("button");
     delBtn.classList.add("delete_btn");
     delBtn.innerHTML = "X";
+    delBtn.addEventListener("click", () => deleteTodo(todo.id));
+
+    // 체크박스
     const checkboxElem = document.createElement("input");
     checkboxElem.setAttribute("type", "checkbox");
 
-    todoItemElem.appendChild(checkboxElem);
-    todoItemElem.appendChild(todoEle);
-    todoItemElem.appendChild(delBtn);
-    todoListElem.appendChild(todoItemElem);
+    todoItemElem.appendChild(checkboxElem); //li 구성 1.checkbox
+    todoItemElem.appendChild(todoEle); //li 구성 2.label(할일입력)
+    todoItemElem.appendChild(delBtn); //li 구성 3.삭제버튼
+
+    todoListElem.appendChild(todoItemElem); // 하나의 li을 ul에 추가
   });
 };
 
 const init = () => {
   $input.addEventListener("keypress", (e) => {
     if (e.keyCode == 13) {
-      //enter키
       appendTodos(e.target.value); //현재입력된 값을 투두리스트에 추가한다
       $input.value = ""; //추가했으면 입력창에 있는 값 지우기
     }
   });
 };
 
-init();
+init(); //index.js가 실행될때 호출
