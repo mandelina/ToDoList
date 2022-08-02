e229a1651aed09f6da48009dfdec7e422682b976
minju moon <mmj9908@gmail.com>
2022-08-03 02:32:17 +0900

    :sparkles: Feat : 시간 및 날짜추가
    Feat : 시간 및 날짜추가

--- js/index.js
+++ js/index.js
@@ -337,3 +337,21 @@ if (localStorage.length > 0) {
 }
 
 init();
+
+//----------------------------------------------------------
+//시간 및 날짜설정
+const TODAY = document.querySelector(".day");
+const TODAY_TIME = document.querySelector(".time");
+// 날짜
+const date = new Date();
+const year = date.getFullYear();
+const month = ('0' + (date.getMonth() + 1)).slice(-2);
+const day = ('0' + date.getDay()).slice(-2);
+const dateStr = year + '-' + month + '-' + day; 
+// 시간
+const hours = ('0' + date.getHours()).slice(-2);
+const minutes = ('0' + date.getMinutes()).slice(-2);
+const seconds = ('0' + date.getSeconds()).slice(-2);
+const timeStr = hours + ':' + minutes  
+TODAY.textContent = dateStr;
+TODAY_TIME.textContent = timeStr;
